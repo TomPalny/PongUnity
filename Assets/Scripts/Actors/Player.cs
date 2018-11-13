ea8af5b922872fe89fe0c0c6b9df54f1ef001be8
Tom Palny <tompalny@gmail.com>
2018-11-13 19:48:32 +0200

    Starting to organize scene

---
+++ Assets/Scripts/Actors/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// Player identifier enum
+    /// </summary>
+    public enum PlayerType
+    {
+        Left,
+        Right
+    }
+
+    #region Editor exposed members
+    [SerializeField] private PlayerType _playerType;
+    [SerializeField] private float _movementSpeed = 5;
+    #endregion
+
+    #region Private members
+    private Transform _transform;
+    private float _halfHeight;
+    #endregion
+
+    private void Start()
+    {
+        // Store highly used variables in advance for performance
+        _transform = transform;
+        _halfHeight = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    private void Update()
+    {
+        // TODO: Get movement input (Make sure left/right player)
+        // TODO: Move player
+        // TODO: Make sure player doesn't leave screen bounds (ScreenUtil.ScreenPhysicalBounds will help you out)
+    }
+}
---
+++ Assets/Scripts/Level/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    #region Editor exposed members
+    [SerializeField] private float _minVelocity = 5;
+    #endregion
+
+    #region Events
+    public event Action<EndZone.EndZoneType> EnteredEndZone;
+    #endregion
+
+    /// <summary>
+    /// Gives the ball a completely random velocity (50%/50% left/right + 50%/50% up/down) with the minimum velocity
+    /// </summary>
+    public void GiveRandomVelocity()
+    {
+        // TODO: Give our rigidbody a random velocity, 50%/50% left/right + 50%/50% up/down - must be at least at _minVelocity
+    }
+
+    /// <summary>
+    /// Resets the ball (position and velocity)
+    /// </summary>
+    public void Reset()
+    {
+        // TODO: Reset our ball's position and velocity
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Make sure if the ball lost velocity that we're never below the minimum
+        if (GetComponent<Rigidbody>().velocity.magnitude < _minVelocity)
+        {
+            float ratio = _minVelocity / GetComponent<Rigidbody>().velocity.magnitude;
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity * ratio;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        // TODO: Handle trigger collisions for endzones
+        // TODO: Make sure we collided with an endzone
+        // TODO: Raise the EnteredEndZone event if we did
+
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
