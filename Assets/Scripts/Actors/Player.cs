9027c687dc8745db8e9318955a7f18a774877117
Tom P <tompalny@gmail.com>
2018-11-13 23:58:12 +0200

    Update Assets/Scripts/Actors/Player.cs
    Made both players move according to keyboard inputs within the bounds of the screen

--- Assets/Scripts/Actors/Player.cs
+++ Assets/Scripts/Actors/Player.cs
@@ -33,5 +33,39 @@ public class Player : MonoBehaviour
         // TODO: Get movement input (Make sure left/right player)
         // TODO: Move player
         // TODO: Make sure player doesn't leave screen bounds (ScreenUtil.ScreenPhysicalBounds will help you out)
+        if (_playerType == PlayerType.Left)
+        {
+            if (Input.GetKey("w"))
+            {
+                if (_transform.position.y + _halfHeight <= ScreenUtil.ScreenPhysicalBounds.yMax)
+                {
+                    _transform.position += Vector3.up * _movementSpeed * Time.deltaTime;
+                }
+            }
+            if (Input.GetKey("s"))
+            {
+                if(_transform.position.y - _halfHeight >= ScreenUtil.ScreenPhysicalBounds.yMin)
+                {
+                    _transform.position += Vector3.down * _movementSpeed * Time.deltaTime;
+                }
+            }
+        }
+        if (_playerType == PlayerType.Right)
+        {
+            if (Input.GetKey("up"))
+            {
+                if (_transform.position.y + _halfHeight <= ScreenUtil.ScreenPhysicalBounds.yMax)
+                {
+                    _transform.position += Vector3.up * _movementSpeed * Time.deltaTime;
+                }
+            }
+            if (Input.GetKey("down"))
+            {
+                if (_transform.position.y - _halfHeight >= ScreenUtil.ScreenPhysicalBounds.yMin)
+                {
+                    _transform.position += Vector3.down * _movementSpeed * Time.deltaTime;
+                }
+            }
+        }
     }
 }
